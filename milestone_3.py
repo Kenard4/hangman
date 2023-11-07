9146a94ed868c620157a192de478c859eb5b8281
Kenard4 <41503750+Kenard4@users.noreply.github.com>
2023-11-07 12:52:31 +0000

    updated initial code to 2 separate functions

---
+++ milestone_3.py
@@ -0,0 +1,29 @@
+def check_guess(guess): # checks if the letter is in the word
+    guess = guess.lower()   # makes the letter lower case
+    if guess in "apple":    # checks if letter is in word
+        print(f"Good guess! {guess} is in the word.") # ends with correct guess
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.") # ends with incorrect guess
+
+def ask_for_input():    # requires user to enter a letter
+    while True: # infinitely loops until it breaks to run check_guess or prints invalid message and comes back here
+        user_input = input("Please enter a single letter: " )
+        if len(user_input) == 1 and user_input.isalpha():   # checks if the letter is a single character and an actual letter
+             break  # if it is, breaks out of while loop and jumps to check_guess function
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.") # if not, goes back to while and repeats
+    check_guess(user_input) # takes the single character into the check_guess function
+        
+ask_for_input()
+
+# initial code:
+# while True:
+#     guess = input("Please enter a single letter: " )
+#     if len(guess) == 1 and guess.isalpha():
+#         break
+#     else:
+#         print("Invalid letter. Please, enter a single alphabetical character.")
+# if guess in "apple":
+#     print(f"Good guess! {guess} is in the word.")
+# else:
+#     print(f"Sorry, {guess} is not in the word. Try again.")
